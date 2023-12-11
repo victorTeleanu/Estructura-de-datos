bdf3266178a2897e8ea5f96b53f923c9b597f318
victorTeleanu <alexteleanu9@gmail.com>
2023-12-11 10:42:11 +0100

    Practicando HashMap

--- src/Telefonos.java
+++ src/Telefonos.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Telefonos {
     public static void main(String[] args) {
         HashMap<String, String> contactos = new HashMap<>();
-        contactos.put("Juan" , "674 592 783");
-        contactos.put("Maria" , "648 901 009");
-        contactos.put("Pedro" , "623 116 740");
-        contactos.put("Fran" , "699 764 532");
-        contactos.put("Julia" , "609 992 125");
+        contactos.put("Juan", "674 592 783");
+        contactos.put("Maria", "648 901 009");
+        contactos.put("Pedro", "623 116 740");
+        contactos.put("Fran", "699 764 532");
+        contactos.put("Julia", "609 992 125");
 
         Scanner miScanner = new Scanner(System.in);
         System.out.println("Dime un contacto y te mostraré su número");
